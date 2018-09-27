c452cc845327f2bf3c01130e650ebbe8995b1553
EC2 Default User <ec2-user@ip-172-31-37-13.us-east-2.compute.internal>
2018-09-27 03:29:34 +0000

    CRUD messages

---
+++ resources/views/messages/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>メッセージ一覧</h1>
+
+    @if (count($messages) > 0)
+       
+    
+    
+    @foreach ($messages as $message)
+                <li>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!} : {{ $message->content }}</li>
+            @endforeach
+    @endif
+    
+            
+    {!! link_to_route("messages.create","新規メッセージの投稿") !!}
+
+
+@endsection
